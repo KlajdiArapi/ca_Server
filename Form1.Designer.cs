2a75522d9d6cf195548b9deb88126dc48a4c22d4
M_inf_5f <klajdi.arapi.studenti@isii.it>
2021-10-29 11:33:31 +0200

    WA_SERVER init

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace wa_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.Chat = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(54, 29);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(86, 20);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.Text = "192.168.3.30";
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(21, 32);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(32, 13);
+            this.lblHost.TabIndex = 1;
+            this.lblHost.Text = "Host:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(171, 32);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(29, 13);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(204, 29);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "8080";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(287, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(83, 28);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(376, 24);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(83, 28);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // Chat
+            // 
+            this.Chat.FormattingEnabled = true;
+            this.Chat.Location = new System.Drawing.Point(24, 90);
+            this.Chat.Name = "Chat";
+            this.Chat.Size = new System.Drawing.Size(435, 134);
+            this.Chat.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(376, 244);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(83, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // txtMex
+            // 
+            this.txtMex.Location = new System.Drawing.Point(24, 247);
+            this.txtMex.Name = "txtMex";
+            this.txtMex.Size = new System.Drawing.Size(346, 20);
+            this.txtMex.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 282);
+            this.Controls.Add(this.txtMex);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.Chat);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.txtHost);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox Chat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMex;
+    }
+}
+
